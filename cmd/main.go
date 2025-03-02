60b62c19809626adb497402a8a8e2d1c001a8dab
Andrei Gushchin <neogan74@gmail.com>
2025-03-02 20:11:46 +0300

    http more

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,20 @@ func one(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello. I'm Leo"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Snippet View"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create Snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", one)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Println("Listening on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
