bcf4224c55f894aae82e347330994acb28b4fb16
Andrei Gushchin <neogan74@gmail.com>
2025-03-02 19:54:01 +0300

    http try

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func one(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello. I'm Leo"))
+}
 
 func main() {
-	fmt.Println("Hello from Snippet box")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", one)
+	log.Println("Listening on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
