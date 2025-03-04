d78db93f9b3119c9f917aec4f23c76d883bee4cf
Andrei Gushchin <neogan74@gmail.com>
2025-03-04 13:07:05 +0300

    http add logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Config struct {
@@ -16,6 +17,9 @@ func main() {
 	flag.StringVar(&cfg.addr, "addr", "localhost:4000", "http service address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
+	// Logging
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir(cfg.staticDir))
@@ -25,7 +29,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Listening on http://%s\n", cfg.addr)
+	infoLog.Printf("Listening on http://%s\n", cfg.addr)
 	err := http.ListenAndServe(cfg.addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
