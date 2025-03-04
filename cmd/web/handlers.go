6711939262cef283505840687f7241a54bedea37
Andrei Gushchin <neogan74@gmail.com>
2025-03-04 13:18:54 +0300

    http add di

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *App) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -28,12 +28,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Println(err)
+		app.errorLog.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *App) snippetView(w http.ResponseWriter, r *http.Request) {
 	// Extract the value of the id parameter from the query string and try to
 	// convert it to an integer using the strconv.Atoi() function. If it can't
 	// be converted to an integer, or the value is less than 1, we return a 404 page // not found response.
@@ -45,7 +45,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d ...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *App) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not.
 	if r.Method != "POST" {
 		// If it's not, use the w.WriteHeader() method to send a 405 status
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,22 +12,34 @@ type Config struct {
 	staticDir string
 }
 
+type App struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	var cfg Config
 	flag.StringVar(&cfg.addr, "addr", "localhost:4000", "http service address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
-	// Logging
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	// DI
+	app := &App{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	// Logging
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir(cfg.staticDir))
-
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	srv := &http.Server{
 		Addr:     cfg.addr,
