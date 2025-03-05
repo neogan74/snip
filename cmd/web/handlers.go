121f4d6b70a6b4a9c88037f09efc2b571203b03f
Andrei Gushchin <neogan74@gmail.com>
2025-03-05 15:25:14 +0300

    add mysql insert and get

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/neogan74/snip/internal/models"
 	"html/template"
 	"net/http"
 	"strconv"
@@ -31,27 +33,39 @@ func (app *App) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *App) snippetView(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id parameter from the query string and try to
-	// convert it to an integer using the strconv.Atoi() function. If it can't
-	// be converted to an integer, or the value is less than 1, we return a 404 page // not found response.
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d ...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "+%v", snippet)
 }
 
 func (app *App) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not.
-	if r.Method != "POST" {
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		//code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
-		w.Header().Set("Allow", "POST")
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create Snippet"))
+
+	title := "0 Snail"
+	content := "0 Snail\nClimb Mount Fuji\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
