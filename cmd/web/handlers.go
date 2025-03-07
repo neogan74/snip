de30c80093c1b58de6d64f40bebb08821b3676c0
Andrei Gushchin <neogan74@gmail.com>
2025-03-07 16:13:54 +0300

    fix: more snippets in home page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,24 +19,26 @@ func (app *App) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	for _, snippet := range snippets {
-		fmt.Fprintln(w, snippet)
+
+	files := []string{
+		"ui/html/base.tmpl.html",
+		"ui/html/partials/nav.tmpl.html",
+		"ui/html/pages/home.tmpl.html",
 	}
-	//files := []string{
-	//	"ui/html/base.tmpl.html",
-	//	"ui/html/partials/nav.tmpl.html",
-	//	"ui/html/pages/home.tmpl.html",
-	//}
-	//ts, err := template.ParseFiles(files...)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//	return
-	//}
-	//
-	//err = ts.ExecuteTemplate(w, "base", nil)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	data := &templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *App) snippetView(w http.ResponseWriter, r *http.Request) {
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/neogan74/snip/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
