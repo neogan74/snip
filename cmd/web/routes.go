03b621e4c6e1e9c5dda78d6e4059c4b3c1e5ab6c
Andrei Gushchin <neogan74@gmail.com>
2025-03-12 19:14:55 +0300

    fix: added middleware for logging

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,5 +12,5 @@ func (app *App) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return secureHeaders(mux)
+	return app.logRequest(secureHeaders(mux))
 }
