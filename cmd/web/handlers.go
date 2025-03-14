cc7d0b797994eadc95922d67f432bd2ac0ae5464
Andrei Gushchin <neogan74@gmail.com>
2025-03-14 19:52:27 +0300

    fix: added middleware for logging

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,6 +13,7 @@ func (app *App) home(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
+
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
