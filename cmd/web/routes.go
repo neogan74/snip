a6be4f185ff54f90ead7a9e474f7b001b0fa9fe1
Andrei Gushchin <neogan74@gmail.com>
2025-06-24 17:49:06 +0300

    feat: embed all html files into binary

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"github.com/neogan74/snip/ui"
 	"net/http"
 )
 
@@ -13,8 +14,8 @@ func (app *App) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	// unprotected wuth auth group of endpoints
 	dynamic := alice.New(app.seesionManager.LoadAndSave, noSurf, app.authenticate)
