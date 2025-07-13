29ca695665e4f1b3c02d8c87fc728b355a6178ff
Andrei Gushchin <neogan74@gmail.com>
2025-07-13 06:40:32 +0300

    feat: all test with test utils now

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,37 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/neogan74/snip/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	app := &App{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
-	ts := httptest.NewTLSServer(app.routes())
+	app := newTestApp(t)
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	code, _, body := ts.get(t, "/ping")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
