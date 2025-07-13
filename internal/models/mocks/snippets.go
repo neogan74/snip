96b8e7e2c11b19b37b137b1fb73e74078ec54b0a
Andrei Gushchin <neogan74@gmail.com>
2025-07-13 06:54:56 +0300

    feat: added mocks

---
+++ internal/models/mocks/snippets.go
@@ -0,0 +1,35 @@
+package mocks
+
+import (
+	"time"
+
+	"github.com/neogan74/snip/internal/models"
+)
+
+var mockSnippet = &models.Snippet{
+	ID:        1,
+	Title:     "On of my favorite",
+	Content:   "This is brilliant note",
+	CreatedAt: time.Now(),
+	ExpiresAt: time.Now(),
+}
+
+type SnippetModel struct{}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 2, nil
+}
+
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	switch id {
+	case 1:
+		return mockSnippet, nil
+	default:
+		return nil, models.ErrNoRecord
+	}
+
+}
+
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return []*models.Snippet{mockSnippet}, nil
+}
