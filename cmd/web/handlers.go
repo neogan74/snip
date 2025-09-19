f0a3ec646930d6236962d31295cb4c894b242d5f
Gushchin Andrei <neogan74@gmail.com>
2025-09-19 13:31:01 +0500

    ref: remove ping

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -214,6 +214,3 @@ func (app *App) userLogoutPost(w http.ResponseWriter, r *http.Request) {
 	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
-}
